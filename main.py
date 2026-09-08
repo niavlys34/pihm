9e2f50d325ee0f2ebc1b754190dcae3e12fb46f2
Sylvain <sylvain@ecomail.fr>
2026-09-08 15:35:48 +0200

    hello world

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("IHM Dashboard")
+        self.resize(800, 480)
+
+        label = QLabel("Hello World")
+        self.setCentralWidget(label)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
